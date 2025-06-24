0f29696fed5ddc058323bce2ffbe541f49183ad6
JoelOgira <joelogira@gmail.com>
2025-06-24 14:26:59 +0300

    Updated styling in the max-width-wrapper

--- components/max-width-wrapper.tsx
+++ components/max-width-wrapper.tsx
@@ -9,9 +9,5 @@ export default function MaxWidthWrapper({
   className,
   children,
 }: MaxWidthWrapperProps) {
-  return (
-    <div className={cn("min-h-[100svh] w-7xl mx-auto px-2.5", className)}>
-      {children}
-    </div>
-  );
+  return <div className={cn("max-w-7xl mx-auto px-2", className)}>{children}</div>;
 }
