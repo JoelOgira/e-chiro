f2e67ade9ba25df7fd1898fdb9213e9d63a3db3c
JoelOgira <joelogira@gmail.com>
2025-07-06 19:31:13 +0300

    Create seed file

---
+++ prisma/seed.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "../lib/generated/prisma";
+import sampleData from "../db/sample-data";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.product.deleteMany()
+    await prisma.product.createMany({
+        data: sampleData.products,
+    })
+
+    console.log("Seeding completed")
+}
+
+main()
+    .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    console.log("Seeding failed")
+    await prisma.$disconnect()
+    process.exit(1)
+  })
