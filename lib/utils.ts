f1ba1bd65925d417bf5867a731659bbf7b30af52
JoelOgira <joelogira@gmail.com>
2025-07-08 20:43:22 +0300

    Added a function to format currency

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export function formatCurrency(num: number): string {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(num);
+}
+
+// // Convert prisma object to regular JS object
+// export function convertToPlainObject<T>(value: T): T {
+//   return JSON.parse(JSON.stringify(value));
+// }
