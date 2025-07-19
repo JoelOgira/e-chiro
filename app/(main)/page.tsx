5e78b164dfaee5436afea02bae7a4ab84d0b5dcc
JoelOgira <joelogira@gmail.com>
2025-07-19 16:11:42 +0300

    Fetching limited data from db

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,11 +2,7 @@ import ProductList from "@/components/shared/product/product-list";
 import { getLatestProducts } from "@/lib/actions/product.actions";
 
 export default async function Home() {
-  const latestProducts = await getLatestProducts();
+  const latestProducts = await getLatestProducts(4);
 
-  return (
-    <>
-      <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
-    </>
-  );
+  return <ProductList data={latestProducts} title="Newest Arrivals" />;
 }
