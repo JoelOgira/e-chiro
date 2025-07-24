7467e77defff1b3772ffb189a197c836dbbf3cc8
JoelOgira <joelogira@gmail.com>
2025-07-24 18:51:31 +0300

    Added sign in schema

--- lib/validators.ts
+++ lib/validators.ts
@@ -14,3 +14,15 @@ export const insertProductSchema = z.object({
   images: z.array(z.string()).min(1, "At least one image is required"),
   banner: z.string().nullable(),
 });
+
+export const signInSchema = z.object({
+  email: z
+    .string({ required_error: "Email is required" })
+    .min(1, "Email is required")
+    .email("Invalid email"),
+  password: z
+    .string({ required_error: "Password is required" })
+    .min(1, "Password is required")
+    .min(6, "Password must be more than 6 characters")
+    .max(32, "Password must be less than 32 characters"),
+});
