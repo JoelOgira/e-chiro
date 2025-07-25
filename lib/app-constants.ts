d4c343519eb5cfd1cbb45361bfe02b96392182c9
JoelOgira <joelogira@gmail.com>
2025-07-25 19:36:32 +0300

    Added sign in default values

--- lib/app-constants.ts
+++ lib/app-constants.ts
@@ -1,3 +1,10 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "e-chiro";
-export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Your one-stop e-commerce store";
-export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Your one-stop e-commerce store";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+
+export const signInDefaultValues = {
+  email: "",
+  password: "",
+};
