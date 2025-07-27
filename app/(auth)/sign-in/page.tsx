67430958d886b2a375364c2a7bf7abe7f6044187
JoelOgira <joelogira@gmail.com>
2025-07-27 19:33:22 +0300

    Created signin page

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,54 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { APP_NAME } from "@/lib/app-constants";
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import CredentialsSignInForm from "./credentials-signin-form";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "Sign In",
+  description: "Login into your e-chiro account",
+};
+
+export default async function SignInPage() {
+  const session = await auth();
+
+  if (session) {
+    redirect("/");
+  }
+
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <Card>
+        <CardHeader className="space-y-3">
+          <Link href="/" className="flex-center">
+            <Image
+              src="/images/logo.svg"
+              width={100}
+              height={100}
+              alt={`${APP_NAME}`}
+              priority={true}
+            />
+          </Link>
+          <CardTitle className="text-center font-semibold text-lg">
+            Sign In
+          </CardTitle>
+          <CardDescription className="text-center">
+            Sign in to your e-chiro account to start shopping
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-3">
+          <CredentialsSignInForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
