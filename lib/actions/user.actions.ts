c2265b07bee29eba85464dc8f33d300b89d1662f
JoelOgira <joelogira@gmail.com>
2025-07-27 19:34:57 +0300

    Updated error handling

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,8 @@
 import { signInSchema } from "../validators";
 import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { AuthError } from "next-auth";
+import { ZodError } from "zod";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -10,19 +12,38 @@ export async function signInWithCredentials(
   formData: FormData
 ) {
   try {
-    const user = signInSchema.parse({
+    const credentials = {
       email: formData.get("email"),
       password: formData.get("password"),
-    });
+    };
+
+    if (!credentials.email) {
+      return { success: false, message: "Email is required!" };
+    }
+
+    if (!credentials.password) {
+      return { success: false, message: "Password is required!" };
+    }
+
+    const user = signInSchema.parse(credentials);
     await signIn("credentials", user);
 
     return { success: true, message: "Signed in successfully" };
   } catch (error) {
+    if (error instanceof AuthError) {
+      return { success: false, message: "Invalid email or password" };
+    }
+
+    if (error instanceof ZodError) {
+      const errors = error.errors.map((e) => e.message);
+      return { success: false, message: errors.join(". ") };
+    }
+
     if (isRedirectError(error)) {
       throw error;
     }
 
-    return { success: false, message: "Invalid email or password" };
+    return { success: false, message: "An unexpected error occurred" };
   }
 }
 
