21e2f99a9700d0c2c5e6e82806a05538a1430710
JoelOgira <joelogira@gmail.com>
2025-07-27 19:34:33 +0300

    Removed default sign in values object

--- lib/app-constants.ts
+++ lib/app-constants.ts
@@ -3,8 +3,3 @@ export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Your one-stop e-commerce store";
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
-
-export const signInDefaultValues = {
-  email: "",
-  password: "",
-};
