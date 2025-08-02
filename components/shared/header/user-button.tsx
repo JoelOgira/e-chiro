d7147a7fae58926fcf7b1cef980c54839ff46fbc
JoelOgira <joelogira@gmail.com>
2025-08-02 17:58:47 +0300

    added callbackUrl to the sign in button

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -1,6 +1,6 @@
+"use client";
+
 import Link from "next/link";
-import { auth } from "@/auth";
-import { signUserOut } from "@/lib/actions/user.actions";
 import { Button } from "@/components/ui/button";
 import { LogOutIcon, User2Icon, UserCheck2 } from "lucide-react";
 import {
@@ -12,9 +12,19 @@ import {
   DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
 
-export default async function UserButton() {
-  const session = await auth();
+import { usePathname, useSearchParams } from "next/navigation";
+import { useSession, signOut } from "next-auth/react";
+
+export default function UserButton() {
+  const { data: session } = useSession();
   const firstName = session?.user?.name?.split(" ")[0] ?? "";
+
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const currentPath = `${pathname}${
+    searchParams.toString() ? "?" + searchParams.toString() : ""
+  }`;
 
   return (
     <>
@@ -43,7 +53,7 @@ export default async function UserButton() {
             <DropdownMenuSeparator />
             <DropdownMenuItem
               className="flex items-center gap-x-3 cursor-pointer"
-              onClick={signUserOut}
+              onClick={() => signOut({ callbackUrl: "/" })}
             >
               <LogOutIcon className="size-5" />
               <span>Sign out</span>
@@ -52,7 +62,9 @@ export default async function UserButton() {
         </DropdownMenu>
       ) : (
         <Button asChild variant="default">
-          <Link href="/sign-in">
+          <Link
+            href={`/sign-in?callbackUrl=${encodeURIComponent(currentPath)}`}
+          >
             <User2Icon className="size-5" />
             <span>Sign In</span>
           </Link>
